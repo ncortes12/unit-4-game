ae0f95175ad2ef366d1da9c2c59df2a28e9c449e
ncortes <ncortes1415@gmail.com>
2018-05-17 18:21:03 -0700

    5-17-18 changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,70 @@
-var computerNumber = Math.floor((Math.random() * 120) + 19)
-var crystalValue1 = Math.floor((Math.random() * 12) + 1)
-var crystalValue2 = Math.floor((Math.random() * 12) + 1)
-var crystalValue3 = Math.floor((Math.random() * 12) + 1)
-var crystalValue4 = Math.floor((Math.random() * 12) + 1)
-var totalScore = []
-var wins = 0
-var losses = 0
-console.log(totalScore)
 $(document).ready(function () {
+    var computerNumber = Math.floor((Math.random() * 120) + 19)
+    var crystalValue1 = Math.floor((Math.random() * 12) + 1)
+    var crystalValue2 = Math.floor((Math.random() * 12) + 1)
+    var crystalValue3 = Math.floor((Math.random() * 12) + 1)
+    var crystalValue4 = Math.floor((Math.random() * 12) + 1)
+    var wins = 0
+    var losses = 0
+    var total = 0
+    var adder = 0
+   
+      
+
+
+
+    
+    var gameOver = function () {
+        total = parseInt(total);
+        console.log("I'm working!");
+        if (total === computerNumber) {
+            wins++;
+            $(".wins").text(wins);
+            
+        }
+        if (total > computerNumber) {
+            losses++;
+            $(".Losses").text(losses);
+           
+        }
+        
+        
+    }
+
 
     $("#randomnumber").append(computerNumber);
 
     $(".crystal1").on("click", function () {
-        alert(crystalValue1);
-        totalScore.push(crystalValue1);
+        adder = crystalValue1;
+        total += adder;
+        $(".totalScore").text(total);
+        gameOver();
     });
 
     $(".crystal2").on("click", function () {
-        alert(crystalValue2);
-        totalScore.push(crystalValue2);
-
+        adder = crystalValue2;
+        total += adder;
+        $(".totalScore").text(total);
+        gameOver();
     });
 
     $(".crystal3").on("click", function () {
-        alert(crystalValue3);
-        totalScore.push(crystalValue3);
+        adder = crystalValue3;
+        total += adder;
+        $(".totalScore").text(total);
+        gameOver();
 
     });
 
     $(".crystal4").on("click", function () {
-        alert(crystalValue4);
-        totalScore.push(crystalValue4);
+        adder = crystalValue4;
+        total += adder;
+        $(".totalScore").text(total);
+        gameOver();
 
     });
+
+
 
 });
 
